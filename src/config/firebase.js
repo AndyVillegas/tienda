0f2f4a30eb7cf75fb66ce64b85f7e08904254c66
AndyVillegas <35319731+AndyVillegas@users.noreply.github.com>
2022-05-19 14:14:13 -0500

    Update firebase.js
    xd

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const config = {
-    apiKey: "AIzaSyAczVmNhlAyhqmr9kAoVg99cnj-DaHwygM",
-    authDomain: "andy-firebase-c65f7.firebaseapp.com",
-    projectId: "andy-firebase-c65f7",
-    storageBucket: "andy-firebase-c65f7.appspot.com",
-    messagingSenderId: "262829679976",
-    appId: "1:262829679976:web:7e47cd0617a311a0e5a2dd"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 firebase.initializeApp(config);
 export default firebase;
